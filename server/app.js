bea87e064cf5427f500071f7a50bde32254ab570
wrouruy <wroyruy@gmail.com>
2025-09-11 19:35:03 +0300

    add excuse-add post endpoint

--- server/app.js
+++ server/app.js
@@ -10,12 +10,26 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 
 mongoose.connect(`mongodb+srv://admin:T1sTDsv2tuqohWzL@cluster0.kijny7k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
-.then(()=> console.log(`Connected to DB`))
+.then(()=> console.log(`Connected to DB`));
+
+const excuseSchema = new mongoose.Schema({
+    autor: String,
+    excuse: String,
+    id: String 
+});
+
+const Excuse = mongoose.model('Excuse', excuseSchema);
 
 app.get(`/`, (req, res)=> {
     res.send(`Hello world!`)    
 })
 
+app.post('/excuse-add', (req, res) => {
+    const {autor, excuse} = req.body;
+    const excuseModel = Excuse({autor: autor, excuse: excuse});
+
+    excuseModel.save();
+});
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port:${PORT}`)
