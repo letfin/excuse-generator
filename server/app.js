a6f12e2d8a9cce48283863ef2fe6b3f310ca1b67
DmutroZel <dimazelenyi01@gmail.com>
2025-09-11 19:15:31 +0300

    add database

--- server/app.js
+++ server/app.js
@@ -1,10 +1,16 @@
 const express = require(`express`);
+const mongoose = require(`mongoose`);
 const app = express();
 const PORT = 3000;
 const bodyParser = require(`body-parser`);
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+
+mongoose.connect(`mongodb+srv://admin:T1sTDsv2tuqohWzL@cluster0.kijny7k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+.then(()=> console.log(`Connected to DB`))
 
 app.get(`/`, (req, res)=> {
     res.send(`Hello world!`)    
@@ -14,3 +20,4 @@ app.get(`/`, (req, res)=> {
 app.listen(PORT, ()=>{
     console.log(`Server is running on port:${PORT}`)
 })
+
